bdca8ca504b93c63efd53c26b6a34f6e606d7c7c
StukC <85483301+StukC@users.noreply.github.com>
2023-12-04 02:49:07 -0500

    logout

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const mysql = require('mysql2');
-
+const session = require('express-session');
 const app = express();
 const port = 3000;
 
@@ -20,6 +20,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Middleware for parsing application/json
 app.use(bodyParser.json());
 
+// Session Middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'default_secret', // Use an environment variable
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Set to true in a production environment with HTTPS
+}));
+
 // Serve static files from the 'Assets' directory
 app.use(express.static('Assets'));
 
@@ -28,6 +36,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/Assets/Login/login.html');
 });
 
+// Login endpoint
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
 
@@ -49,29 +58,51 @@ app.post('/login', (req, res) => {
         return res.status(401).send('Password is incorrect');
       }
 
-      // User authenticated, check user's title
+      // User authenticated, establish a session
+      req.session.user = {
+        id: results[0].UserID,
+        username: username,
+        title: results[0].Title
+      };
+
+      // Redirect based on user's title
       const userTitle = results[0].Title;
       switch (userTitle) {
         case 'Admin':
           res.redirect('/SysAdmin/admin-dashboard.html');
           break;
         case 'Reviewer':
-          res.redirect('/Reviewer/reviewer-dashboard.html'); 
+          res.redirect('/Reviewer/reviewer-dashboard.html');
           break;
         case 'Author':
-          res.redirect('/Author/author-dashboard.html'); 
+          res.redirect('/Author/author-dashboard.html');
           break;
         case 'Chair':
-          res.redirect('/Chair/program-chair-dashboard.html'); 
+          res.redirect('/Chair/program-chair-dashboard.html');
           break;
         default:
-          res.send('NA.');
+          res.send('No appropriate dashboard found for user.');
       }      
     } catch (bcryptError) {
       console.error('Error comparing passwords:', bcryptError);
       res.status(500).send('Server error during password comparison');
     }
   });
+});
+
+// Logout endpoint
+app.get('/logout', (req, res) => {
+  if (req.session.user) {
+    req.session.destroy((err) => {
+      if(err) {
+          console.error('Error destroying session:', err);
+          return res.status(500).send('Could not log out, server error');
+      }
+      res.redirect('/Login/login.html'); // Redirect to login page after logout
+    });
+  } else {
+    res.redirect('/Login/login.html'); // If no session, directly redirect
+  }
 });
 
 // Start the server
